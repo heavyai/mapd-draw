39fb7f86d804819cebaabc557c4f398a16be020c
Ricky <evholtz@gmail.com>
2017-04-28 15:23:35 -0700

    Linted (#7)

--- src/core/vec2d.js
+++ src/core/vec2d.js
@@ -8,9 +8,7 @@ import {vec2 as Vec2d} from "gl-matrix"
  * @param  {Vec2d} v2
  * @return {Number}    z coord of the 2D crossproduct
  */
-Vec2d.cross2d = (v1, v2) => {
-  return v1[0] * v2[1] - v1[1] * v2[0]
-}
+Vec2d.cross2d = (v1, v2) => v1[0] * v2[1] - v1[1] * v2[0]
 
 /**
  * Calculates the angle between two vectors when directionality
@@ -30,7 +28,7 @@ Vec2d.angleFast = (v1, v2) => Math.acos(Vec2d.dot(v1, v2))
 Vec2d.angle = (v1, v2) => {
   const c = Vec2d.dot(v1, v2)
   const s = Vec2d.cross2d(v1, v2)
-  let angle = Math.atan2(s, c)
+  const angle = Math.atan2(s, c)
   return angle
 }
 
